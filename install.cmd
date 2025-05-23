152792f4d42483f13b338e1eaed547d65d669834
GoombaStudios <140081099+GoombaStudios@users.noreply.github.com>
2025-05-23 22:26:22 +0200

    Add files via upload

---
+++ install.cmd
@@ -0,0 +1,23 @@
+@echo off
+setlocal enabledelayedexpansion
+title RGBC Installer
+set oldcd=%cd%
+powershell -command "iwr https://raw.githubusercontent.com/GoombaStudios/RGBC/refs/heads/code/rgbc.cmd -outfile rgbc.cmd"
+
+cd C:\Windows\System32
+echo test>test.test
+if not exist test.test (
+    cls
+    echo Please run this script as administrator.
+    pause
+    exit
+)
+
+del test.test
+
+move %oldcd%\rgbc.cmd C:\Windows\System32\rgbc.cmd
+
+cls
+echo RGBC Successfully installed.
+pause
+exit
